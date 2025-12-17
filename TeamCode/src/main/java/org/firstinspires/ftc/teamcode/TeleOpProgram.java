a0ba3b3e069e46ea2aa11c3077baf0201e549def
Hellen Isakov <hellen.isakov@gmail.com>
2025-12-17 22:21:25 +0200

    new commit test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpProgram.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@TeleOp (name = "TeleOpProgram")
+    public class TeleOpProgram extends NextFTCOpMode {
+        public TeleOpProgram() {
+            addComponents(
+                    new SubsystemComponent(DriveSubsystem.INSTANCE),
+                    BulkReadComponent.INSTANCE,
+                    BindingsComponent.INSTANCE
+            );
+        }
+
+    @Override
+    public void onStartButtonPressed() {
+
+        // -------------------------DRIVE-------------------------
+        Command driverControlled = DriveSubsystem.INSTANCE.driverControlled;
+        driverControlled.schedule();
+
+        // -------------------------INTAKE-------------------------
+        /*Gamepads.gamepad1().a()
+                .whenBecomesTrue(IntakeSubsystem.INSTANCE.intakePow(0));
+
+        Gamepads.gamepad1().b()
+                .whenBecomesTrue(IntakeSubsystem.INSTANCE.intakePow(1));*/
+    }
+
+}
+
