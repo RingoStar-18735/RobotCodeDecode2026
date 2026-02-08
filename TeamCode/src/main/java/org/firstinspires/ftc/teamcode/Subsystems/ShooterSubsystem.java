0d814d043b143568edccd6270b786a21f10edbe1
Hellen Isakov <hellen.isakov@gmail.com>
2026-02-08 22:36:50 +0200

    17.2.26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
@@ -49,6 +49,7 @@ public class ShooterSubsystem implements Subsystem {
  //       return new RunToVelocity(PID, v).requires(this);
   //  }
 
+
     public Command RunVelocity(double v){
         return new InstantCommand(
                 ()->  PID.setTarget(v)
