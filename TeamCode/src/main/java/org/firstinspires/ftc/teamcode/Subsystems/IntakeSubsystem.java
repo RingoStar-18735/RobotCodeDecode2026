ad6e64f25715e054bd5249def371f45e4e0c952c
yogevgold <145796130+yogevgold@users.noreply.github.com>
2026-05-08 15:10:03 +0300

    Update IntakeSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeSubsystem.java
@@ -3,31 +3,29 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import org.firstinspires.ftc.teamcode.RobotMap;
 
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.ftc.ActiveOpMode;
-import dev.nextftc.hardware.impl.CRServoEx;
 import dev.nextftc.hardware.impl.MotorEx;
 
 public class IntakeSubSystem implements Subsystem {
     public final static IntakeSubSystem INSTANCE = new IntakeSubSystem();
     public IntakeSubSystem() {}
 
-    public MotorEx IntakeMotor = new MotorEx("1E").reversed();
+    public MotorEx IntakeMotor = new MotorEx("2C");
 
-    public CRServoEx CR_left = new CRServoEx("02E");
-    public CRServoEx CR_right = new CRServoEx("03E");
+    public MotorEx TransferMotor = new MotorEx("3C").brakeMode();
 
-    private void TransferPower(double power){
-        CR_left.setPower(power);
-        CR_right.setPower(-power);
+    private void TransferPower(double pow){
+        TransferMotor.setPower(pow);
     }
 
     @Override
     public void periodic() {
-        ActiveOpMode.telemetry().addData("IntakeMotor Power:" , IntakeMotor.getPower());
-        ActiveOpMode.telemetry().addData("IntakeServo Power:" , CR_left.getPower());
+//        ActiveOpMode.telemetry().addData("IntakeMotor Power:" , IntakeMotor.getPower());
+//        ActiveOpMode.telemetry().addData("Transition Power:" , TransferMotor.getPower());
 
     }
 
@@ -40,37 +38,38 @@ public class IntakeSubSystem implements Subsystem {
 
 
     public Command Transfer(double pow) {
-        return new LambdaCommand().
-                setStart(()-> TransferPower(pow))
-                .setIsDone( ()-> CR_left.getPower() == pow && CR_right.getPower() == pow)
+        return new LambdaCommand()
+                .setStart(()-> TransferPower(pow))
+                .setIsDone( ()-> TransferMotor.getPower() == pow)
                 .requires(this);
     }
+
     public Command IntakeFullyTransfer(){
         return new ParallelGroup(
-                Transfer(RobotMap.TRANSFER_SERVO_POWER),
+                Transfer(RobotMap.TRANSMISSION_MOTOR_POWER),
                 IntakePower(RobotMap.INTAKE_MOTOR_POWER)
         );
     }
 
     public Command IntakeFullyNotTransfer(){
         return new ParallelGroup(
+                new Delay(3),
                 IntakePower(RobotMap.INTAKE_MOTOR_POWER),
-                Transfer(-RobotMap.TRANSFER_SERVO_POWER),
-                IntakePower(RobotMap.INTAKE_MOTOR_POWER),
-                Transfer(-RobotMap.TRANSFER_SERVO_POWER)
+                Transfer(RobotMap.TRANSMISSION_MOTOR_REVERSE_POWER)
                 );
     }
 
     public Command IntakeStop(){
-        return new ParallelGroup(
-                IntakePower(0.0),
-                Transfer(0.0)
+        return new SequentialGroup(
+                Transfer(0.0),
+                new Delay(0.1),
+                IntakePower(0.0)
         ).setName("קומנד הפסיק אינטייק");
     }
 
     public Command ReversedIntake() {
         return new ParallelGroup(
-            Transfer(-RobotMap.TRANSFER_SERVO_POWER),
+            Transfer(-RobotMap.TRANSMISSION_MOTOR_POWER),
             IntakePower(RobotMap.INTAKE_REVERSED_POWER)
         );
     }
