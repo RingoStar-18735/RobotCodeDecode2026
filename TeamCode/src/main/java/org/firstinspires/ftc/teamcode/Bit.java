cd0c21ce4c6f23f5bb856428a1ee0310b081e2fd
Hellen-Isakov <hellen.isakov@gmail.com>
2026-05-18 20:50:38 +0300

    18.5.26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bit.java
@@ -1,66 +1,66 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubSystem;
-import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.TurretSubsystem;
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.groups.SequentialGroup;
-import dev.nextftc.core.components.BindingsComponent;
-import dev.nextftc.core.components.SubsystemComponent;
-import dev.nextftc.extensions.pedro.PedroComponent;
-import dev.nextftc.extensions.pedro.PedroDriverControlled;
-import dev.nextftc.ftc.Gamepads;
-import dev.nextftc.ftc.NextFTCOpMode;
-import dev.nextftc.ftc.components.BulkReadComponent;
-import dev.nextftc.hardware.driving.DriverControlledCommand;
-
-
-@TeleOp (name = "BitCode")
-public class Bit extends NextFTCOpMode {
-
-    public Bit(){
-        addComponents(
-                new SubsystemComponent(
-                        ShooterSubsystem.INSTANCE,
-                        IntakeSubSystem.INSTANCE,
-                        TurretSubsystem.INSTANCE
-                ),
-
-                new PedroComponent(Constants::createFollower),
-                BulkReadComponent.INSTANCE,
-                BindingsComponent.INSTANCE
-        );
-    }
-
-    public Command BitCommand(){
-        return new SequentialGroup(
-                ShooterSubsystem.INSTANCE.move(1),
-                ShooterSubsystem.INSTANCE.ServoAim(0.6), // CLOSE
-                ShooterSubsystem.INSTANCE.ServoAim(0.45), // MID
-                ShooterSubsystem.INSTANCE.ServoAim(0.65), // FAR
-                IntakeSubSystem.INSTANCE.IntakeFullyTransfer(),
-                TurretSubsystem.INSTANCE.ResetAngle()
-            //    TurretSubsystem.INSTANCE.LimelightMove()
-        );
-    }
-
-    @Override
-    public void onStartButtonPressed() {
-
-        DriverControlledCommand driverControlled = new PedroDriverControlled(
-                Gamepads.gamepad1().leftStickY(),
-                Gamepads.gamepad1().leftStickX(),
-                Gamepads.gamepad1().rightStickX().negate(),
-                false
-        );
-        driverControlled.schedule();
-
-        Gamepads.gamepad1().a()
-                .whenTrue(BitCommand());
-    }
-
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubSystem;
+//import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
+//import org.firstinspires.ftc.teamcode.Subsystems.TurretSubsystem;
+//import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+//
+//import dev.nextftc.core.commands.Command;
+//import dev.nextftc.core.commands.groups.SequentialGroup;
+//import dev.nextftc.core.components.BindingsComponent;
+//import dev.nextftc.core.components.SubsystemComponent;
+//import dev.nextftc.extensions.pedro.PedroComponent;
+//import dev.nextftc.extensions.pedro.PedroDriverControlled;
+//import dev.nextftc.ftc.Gamepads;
+//import dev.nextftc.ftc.NextFTCOpMode;
+//import dev.nextftc.ftc.components.BulkReadComponent;
+//import dev.nextftc.hardware.driving.DriverControlledCommand;
+//
+//
+//@TeleOp (name = "BitCode")
+//public class Bit extends NextFTCOpMode {
+//
+//    public Bit(){
+//        addComponents(
+//                new SubsystemComponent(
+//                        ShooterSubsystem.INSTANCE,
+//                        IntakeSubSystem.INSTANCE,
+//                        TurretSubsystem.INSTANCE
+//                ),
+//
+//                new PedroComponent(Constants::createFollower),
+//                BulkReadComponent.INSTANCE,
+//                BindingsComponent.INSTANCE
+//        );
+//    }
+//
+//    public Command BitCommand(){
+//        return new SequentialGroup(
+//                ShooterSubsystem.INSTANCE.move(1),
+//                ShooterSubsystem.INSTANCE.ServoAim(0.6), // CLOSE
+//                ShooterSubsystem.INSTANCE.ServoAim(0.45), // MID
+//                ShooterSubsystem.INSTANCE.ServoAim(0.65), // FAR
+//                IntakeSubSystem.INSTANCE.IntakeFullyTransfer(),
+//                TurretSubsystem.INSTANCE.ResetAngle()
+//            //    TurretSubsystem.INSTANCE.LimelightMove()
+//        );
+//    }
+//
+//    @Override
+//    public void onStartButtonPressed() {
+//
+//        DriverControlledCommand driverControlled = new PedroDriverControlled(
+//                Gamepads.gamepad1().leftStickY(),
+//                Gamepads.gamepad1().leftStickX(),
+//                Gamepads.gamepad1().rightStickX().negate(),
+//                false
+//        );
+//        driverControlled.schedule();
+//
+//        Gamepads.gamepad1().a()
+//                .whenTrue(BitCommand());
+//    }
+//
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
@@ -46,8 +46,8 @@ public class ShooterSubsystem implements Subsystem {
 
     public final MotorEx Shooter = new MotorEx("0C");
     public final PIDController PID = new PIDController(RobotMap.SHOOTER_P, RobotMap.SHOOTER_I, RobotMap.SHOOTER_D);
-    public final ServoEx left_aim = new ServoEx("00E");
-    public final ServoEx right_aim = new ServoEx("01E");
+    public final ServoEx left_aim = new ServoEx("02E");
+    public final ServoEx right_aim = new ServoEx("03E");
 
 
 
@@ -74,7 +74,6 @@ public class ShooterSubsystem implements Subsystem {
                     } else {
                         ServoPos = RobotMap.SERVO_MOVE_MID;
                     }
-
                     left_aim.setPosition(ServoPos);
                     right_aim.setPosition(1 - ServoPos);
                 })
@@ -120,12 +119,12 @@ public class ShooterSubsystem implements Subsystem {
     }
 
     public Command ServoAimCommand(double pos) {
-        return new SetPositions(left_aim.to(1 - pos), right_aim.to(pos));
+        return new SetPositions(left_aim.to(pos), right_aim.to(1 - pos));
     }
 
     public void ServoAim(double pos) {
-        left_aim.setPosition(1 - pos);
-        right_aim.setPosition(pos);
+        left_aim.setPosition(pos);
+        right_aim.setPosition(1 - pos);
     }
 
 //    public Command ServoAutoAim(Pose RobotPose, Pose TargetPose) {
