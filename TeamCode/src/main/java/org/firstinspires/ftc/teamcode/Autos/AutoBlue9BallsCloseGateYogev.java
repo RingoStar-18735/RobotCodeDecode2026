fe76806bf14c527c761d3787bb9bf33f3e0b9513
yogevgold <145796130+yogevgold@users.noreply.github.com>
2026-06-18 09:44:16 +0200

    18.6
    9:44

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/AutoBlue9BallsCloseGateYogev.java
@@ -0,0 +1,363 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.FieldMap;
+import org.firstinspires.ftc.teamcode.Printer;
+import org.firstinspires.ftc.teamcode.RobotBank;
+import org.firstinspires.ftc.teamcode.RobotMap;
+import org.firstinspires.ftc.teamcode.Subsystems.AllianceType;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubSystem;
+import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.TurretSubsystem;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.DrawingRobot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelDeadlineGroup;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@Autonomous
+public class AutoBlue9BallsCloseGateYogev extends NextFTCOpMode {
+    public PathChain Path1;
+    public PathChain Path2;
+    public PathChain Path3;
+    public PathChain Path4;
+    public PathChain Path5;
+    public PathChain Path6;
+    public PathChain Path7;
+    public PathChain Path8;
+/*    public PathChain Path9;
+    public PathChain Path10;
+    public PathChain Path11;*/
+    boolean hasStarted = false;
+    boolean ServoActivateAuto = false;
+    double distance;
+
+    Command Path1Command;
+    Command Path2Command;
+    Command Path3Command;
+    Command Path4Command;
+    Command Path5Command;
+    Command Path6Command;
+    Command Path7Command;
+    Command Path8Command;
+//  Command Path9Command;
+    Command Path10Command;
+    Command Path11Command;
+    Command Auto;
+    Follower follower;
+    List<String> a = new ArrayList<String>();
+
+
+    public AutoBlue9BallsCloseGateYogev(){
+
+        addComponents(
+                new SubsystemComponent(
+                        ShooterSubsystem.INSTANCE,
+                        IntakeSubSystem.INSTANCE,
+                        TurretSubsystem.INSTANCE,
+                        Printer.INSTANCE
+                ),
+                new PedroComponent(Constants::createFollower),
+                BulkReadComponent.INSTANCE,
+
+                BindingsComponent.INSTANCE
+        );
+
+    }
+
+    public Command Shooter() {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    telemetry.addLine("SHOOT COMMAND STARTED");
+                    telemetry.addData("vel: ", -ShooterSubsystem.INSTANCE.Shooter.getVelocity());
+                    telemetry.addData("Speed: ", ShooterSubsystem.INSTANCE.ShooterSpeed);
+                })
+                .setUpdate(() -> {
+                    if (Math.abs(ShooterSubsystem.INSTANCE.Shooter.getVelocity()) >= ShooterSubsystem.INSTANCE.ShooterSpeed - 200) {
+                        telemetry.addData("אני2: ", -ShooterSubsystem.INSTANCE.Shooter.getVelocity());
+                        IntakeSubSystem.INSTANCE.IntakeMotor.setPower(RobotMap.INTAKE_MOTOR_POWER);
+                        IntakeSubSystem.INSTANCE.TransferMotor.setPower(RobotMap.TRANSMISSION_MOTOR_POWER);
+
+                    } else {
+                        IntakeSubSystem.INSTANCE.IntakeMotor.setPower(0);
+                        IntakeSubSystem.INSTANCE.TransferMotor.setPower(0);
+                    }
+                })
+                .setIsDone(()-> false)
+                .requires(IntakeSubSystem.INSTANCE);
+    }
+
+    public Command Shoot() {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    telemetry.addLine("SHOOT COMMAND STARTED");
+                    telemetry.addData("vel: ", -ShooterSubsystem.INSTANCE.Shooter.getVelocity());
+                    telemetry.addData("Speed: ", ShooterSubsystem.INSTANCE.ShooterSpeed);
+                })
+                .setUpdate(() -> {
+                    if (Math.abs(ShooterSubsystem.INSTANCE.Shooter.getVelocity()) >= ShooterSubsystem.INSTANCE.ShooterSpeed - 500) {
+                        telemetry.addData("אני2: ", -ShooterSubsystem.INSTANCE.Shooter.getVelocity());
+                        IntakeSubSystem.INSTANCE.IntakeMotor.setPower(1);
+                        IntakeSubSystem.INSTANCE.TransferMotor.setPower(1);
+                    }
+                    else {
+                        new SequentialGroup(
+                                new InstantCommand(()-> IntakeSubSystem.INSTANCE.IntakeMotor.setPower(0)),
+                                new InstantCommand(()-> IntakeSubSystem.INSTANCE.TransferMotor.setPower(0)),
+                                new Delay(1)
+                        );
+                    }
+                })
+                .setIsDone(()-> false)
+                .requires(IntakeSubSystem.INSTANCE);
+    }
+    Command shootSequence = new SequentialGroup(
+            new ParallelDeadlineGroup(
+                    new Delay(2),
+                    ShooterSubsystem.INSTANCE.RunFullSpeed(() ->
+                            Math.sqrt(
+                                    Math.pow(FieldMap.BLUE_TARGET_POS.getX() - follower.getPose().getX(), 2) +
+                                            Math.pow(FieldMap.BLUE_TARGET_POS.getY() - follower.getPose().getY(), 2)
+                            )
+                    )
+            ),
+            new ParallelDeadlineGroup(
+                    new Delay(2),
+                    Shoot()
+            )
+//            ShooterSubsystem.INSTANCE.StopSpeed()
+    );
+
+
+    @Override
+    public void onInit() {
+        RobotMap.TURRET_ROBOT_DIFRANCE = false;
+        RobotBank.Alliance = AllianceType.BLUE;
+        TurretSubsystem.INSTANCE.ResetAngleRight().schedule();
+        ShooterSubsystem.INSTANCE.Shooter.setPower(0);
+        IntakeSubSystem.INSTANCE.IntakeStop().schedule();
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(34, 135, Math.toRadians(180)));
+        telemetry.addData("pos: ", follower.getPose());
+//        Path1 = follower.pathBuilder().addPath(
+//                        new BezierLine(
+//                                new Pose(34.000, 135.000),
+//
+//                                new Pose(58.000, 84.000)
+//                        )
+//                ).setConstantHeadingInterpolation(Math.toRadians(180))
+//
+//                .build();
+//
+//        Path2 = follower.pathBuilder().addPath(
+//                        new BezierCurve(
+//                                new Pose(58.000, 84.000),
+//                                new Pose(-55.000, 90.834),
+//                                new Pose(131.761, 64.579),
+//                                new Pose(-75.000, 90.000),
+//                                new Pose(55.240, 52.242),
+//                                new Pose(59.000, 85.000)
+//                        )
+//                ).setConstantHeadingInterpolation(Math.toRadians(180))
+//
+//                .build();
+//
+//        Path3 = follower.pathBuilder().addPath(
+//                        new BezierCurve(
+//                                new Pose(59.000, 85.000),
+//                                new Pose(69.787, 38.278),
+//                                new Pose(-110.000, 60.887),
+//                                new Pose(180.000, 65.574),
+//                                new Pose(-72.000, 84.000),
+//                                new Pose(31.769, 64.318),
+//                                new Pose(58.000, 84.000)
+//                        )
+//                ).setConstantHeadingInterpolation(Math.toRadians(180))
+//
+//                .build();
+//
+//        Path4 = follower.pathBuilder().addPath(
+//                        new BezierLine(
+//                                new Pose(58.000, 84.000),
+//
+//                                new Pose(26.562, 69.479)
+//                        )
+//                ).setConstantHeadingInterpolation(Math.toRadians(180))
+//
+//                .build();
+
+
+        Path1 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(34.000, 134.000),
+
+                                new Pose(56.000, 83.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+
+        Path2 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(56.000, 83.000),
+                                new Pose(22.832, 86.222),
+                                new Pose(-10.000, 83.000),
+                                new Pose(63.339, 75.605),
+                                new Pose(20.000, 75.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+
+        Path3 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(20.000, 75.000),
+                                new Pose(52.991, 73.100),
+                                new Pose(56.000, 83.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+
+        Path4 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(56.000, 83.000),
+                                new Pose(63.832, 54.977),
+                                new Pose(17.065, 52.569),
+                                new Pose(-10.000, 57.771),
+                                new Pose(57.626, 66.088),
+                                new Pose(20.000, 68.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+        Path5 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(20.000, 68.000),
+                                new Pose(54.618, 65.989),
+                                new Pose(56.000, 83.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+        Path6 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 83.000),
+
+                                new Pose(35.000, 69.000)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(180))
+
+                .build();
+
+
+
+        Path1Command = new FollowPath(Path1);
+        Path2Command = new FollowPath(Path2);
+        Path3Command = new FollowPath(Path3);
+        Path4Command = new FollowPath(Path4);
+        Path5Command = new FollowPath(Path5);
+        Path6Command = new FollowPath(Path6);
+//        Path7Command = new FollowPath(Path7);
+//        Path8Command = new FollowPath(Path8);
+//        Path9Command = new FollowPath(Path9);
+//        Path10Command = new FollowPath(Path10);
+//        Path11Command = new FollowPath(Path11);
+
+
+        Auto = new SequentialGroup(
+                Path1Command,
+                shootSequence,
+                ShooterSubsystem.INSTANCE.StopSpeed(),
+                MoveWithoutShooting(Path2Command),
+                MoveWithoutShooting(Path3Command),
+                shootSequence,
+                ShooterSubsystem.INSTANCE.StopSpeed(),
+                MoveWithoutShooting(Path4Command),
+                MoveWithoutShooting(Path5Command),
+                shootSequence,
+                ShooterSubsystem.INSTANCE.StopSpeed(),
+                MoveWithoutShooting(Path6Command)
+        );
+    }
+
+    private Command MoveWhilePathing(Command path) {
+        return new ParallelDeadlineGroup(
+                new Delay(4),
+                path,
+               IntakeSubSystem.INSTANCE.IntakeFullyNotTransfer()
+        );
+    }
+
+    private Command MoveWithoutShooting(Command path) {
+        ShooterSubsystem.INSTANCE.StopSpeed();
+        return new SequentialGroup(
+                new ParallelGroup(
+                        path,
+                        IntakeSubSystem.INSTANCE.IntakePower(0.6),
+                        IntakeSubSystem.INSTANCE.Transfer(-1)
+                ),
+                new Delay(2),
+                IntakeSubSystem.INSTANCE.IntakePower(0.2)
+        );
+
+    }
+
+    @Override
+    public void onUpdate() {
+        follower.update();
+        distance = Math.sqrt(Math.pow(FieldMap.BLUE_TARGET_POS.getX() - follower.getPose().getX(), 2) + Math.pow(FieldMap.BLUE_TARGET_POS.getY() - follower.getPose().getY(), 2));
+        if (hasStarted){
+            TurretSubsystem.INSTANCE.FollowPoint(FieldMap.BLUE_TARGET_POS, follower.getPose()).schedule();
+        }
+        RobotBank.Offset = TurretSubsystem.INSTANCE.getOffset();
+        RobotBank.LastAutoPos = follower.getPose();
+        RobotBank.LastAutoTurretAngle = TurretSubsystem.INSTANCE.getAngle();
+        DrawingRobot.drawDebug(follower);
+        DrawingRobot.drawPoseHistory(follower.getPoseHistory());
+        telemetry.addData("pose: ", RobotBank.LastAutoPos);
+        telemetry.addData("yaw: ", follower.getPose().getHeading());
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        hasStarted = true;
+        TurretSubsystem.INSTANCE.setToFollow(true);
+        ShooterSubsystem.INSTANCE.ServoMoveByField(() ->
+                Math.sqrt(
+                        Math.pow(FieldMap.BLUE_TARGET_POS.getX() - follower.getPose().getX(), 2) +
+                                Math.pow(FieldMap.BLUE_TARGET_POS.getY() - follower.getPose().getY(), 2)
+                )
+        ).schedule();
+        Auto.schedule();
+        RobotBank.Offset = TurretSubsystem.INSTANCE.getAngle();
+        RobotBank.LastAutoPos = follower.getPose();
+        RobotBank.LastAutoTurretAngle = TurretSubsystem.INSTANCE.getAngle();
+    }
+}
